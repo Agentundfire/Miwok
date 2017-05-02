ef450ab10f5419dc3de7ae11b50c23e5fcb0f0f3
RJ <201213388@ingenieria.usac.edu.gt>
2017-05-01 23:38:57 -0600

    Fill Java: ColorsActivity, FamilyActivity and PhrasesActivity. Then unify everything into one word_list.xml instead of 4 xml.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,11 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,20 +11,20 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
         //create list of words
         ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("lutti","one"));
-        words.add(new Word("lutti","two"));
-        words.add(new Word("lutti","three"));
-        words.add(new Word("lutti","four"));
-        words.add(new Word("lutti","five"));
-        words.add(new Word("lutti","six"));
-        words.add(new Word("lutti","seven"));
-        words.add(new Word("lutti","eight"));
-        words.add(new Word("lutti","nine"));
-        words.add(new Word("lutti","ten"));
+        words.add(new Word("otiiko","two"));
+        words.add(new Word("tolookosu","three"));
+        words.add(new Word("oyyisa","four"));
+        words.add(new Word("massokka","five"));
+        words.add(new Word("temmokka","six"));
+        words.add(new Word("kenekaku","seven"));
+        words.add(new Word("kawinta","eight"));
+        words.add(new Word("wo'e","nine"));
+        words.add(new Word("na'aacha","ten"));
 
         // Create an {@link ArrayAdapter}, whose data source is a list of Word.java. The
         // adapter knows how to create layouts for each item in the list, using the
@@ -38,7 +34,7 @@ public class NumbersActivity extends AppCompatActivity {
         WordAdapter wordAdapter = new WordAdapter(this,words);
 
         // Get a reference to the ListView, and attach the adapter to the listView.
-        ListView listView = (ListView) findViewById(R.id.list_view_numbers);
+        ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(wordAdapter);
 
     }
